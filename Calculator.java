f67787d4c849197baa59bb25394384d320e574b4
Maksim Fiodarau <m.fiodarau@andersenlab.com>
2022-05-31 14:14:32 +0300

    refactoring methods

--- Calculator.java
+++ Calculator.java
@@ -1,40 +1,43 @@
 public class Calculator {
     public static void main(String[] args) {
 
-        System.out.println(addition(10, 5));
-        System.out.println(subtraction(10, 5));
-        System.out.println(multiplication(10, 5));
-        System.out.println(division(10, 5));
-        System.out.println(division(10, 0));
-        System.out.println(remainderOfTheDivision(11, 5));
-        System.out.println(exponentiation(2, 3));
+        System.out.println(Operation.addition(10, 5));
+        System.out.println(Operation.subtraction(10, 5));
+        System.out.println(Operation.multiplication(10, 5));
+        System.out.println(Operation.division(10, 5));
+        System.out.println(Operation.division(10, 0));
+        System.out.println(Operation.remainderOfTheDivision(11, 5));
+        System.out.println(Operation.exponentiation(2, 3));
+        System.out.println(Operation.addition(100, 100));
 
     }
 
-    static double addition (double num1, double num2) {
-        return num1 + num2;
-    }
+        class Operation {
+            private static double addition (double num1, double num2) {
+                 return num1 + num2;
+        }
 
-    static double subtraction (double num1, double num2) {
-        return num1 - num2;
-    }
+            private static double subtraction (double num1, double num2) {
+                return num1 - num2;
+        }
 
-    static double multiplication (double num1, double num2) {
-        return num1 * num2;
-    }
+            private static double multiplication (double num1, double num2) {
+                return num1 * num2;
+        }
 
-    static double division (double num1, double num2) {
-        if (num2 == 0)
-            return 0;
-        else 
-            return num1 / num2;
-    }
+            private static double division (double num1, double num2) {
+                if (num2 == 0)
+                    return 0;
+                else 
+                    return num1 / num2;
+        }
     
-    static double remainderOfTheDivision (double num1, double num2) {
-        return num1 % num2;
-    }
+            private static double remainderOfTheDivision (double num1, double num2) {
+                return num1 % num2;
+        }
 
-    static double exponentiation (double num1, double num2) {
-        return Math.pow(num1, num2);
+            private static double exponentiation (double num1, double num2) {
+                return Math.pow(num1, num2);
+        }
     }
 }
