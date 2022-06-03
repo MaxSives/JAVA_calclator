2a53c6c0102eb573165d1be6d7fc3cd7775312e7
Maksim Fiodarau <m.fiodarau@andersenlab.com>
2022-06-03 15:11:16 +0300

    delete method main

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,10 +1,5 @@
 public class Calculator {
 
-    public static void main(String[] args) {
-        System.out.println(calculator(2, '+', 3));
-    }
-
-
     public static double calculator(double a, char operation, double b) {
         double result = 0;
         switch (operation){
